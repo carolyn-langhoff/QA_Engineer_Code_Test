2b04b1e40ea09140fa229f164564560130a0bbeb
carolyn <carolyn_langhoff@outlook.com>
2022-05-18 19:41:21 -0500

    Commit

---
+++ test_engineer/todo/src/ProgrammingChallenge.py
@@ -0,0 +1,33 @@
+#This is a Python program written by Carolyn Langhoff to test the To-Do Application
+
+#Imports
+
+
+#Variables
+
+
+#Arrays
+
+
+
+#Create Class
+class ProgrammingTest:
+
+    #Test add button
+
+
+
+    #Test remove button
+
+
+
+    #Test check-boxes
+
+
+
+    #Test various browsers
+
+
+
+
+    #Test functionality by creating a To-Do list
