40593718096bf01ba0eb389944e11129c6024454
carolyn <carolyn_langhoff@outlook.com>
2022-05-20 14:15:01 -0500

    CodeTest.py now opens the Todo application.

--- test_engineer/CodeTest.py
+++ test_engineer/CodeTest.py
@@ -1,17 +1,27 @@
 # This is Carolyn's attempt at automating testing of the Todo application for Open Drives.
 
 #Imports
+from codeop import CommandCompiler
+from msilib.schema import LaunchCondition, SelfReg
 import time
+from urllib.request import urlopen
 from selenium import webdriver
+from setuptools import Command
+import urllib3
 from webdriver_manager.firefox import GeckoDriverManager
+
+
 
 
  #The following is a test to make sure selenium, node js, etc. are running correctly
 
-#Opens the firefox browser
+#Opens the firefox browser and the application
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 driver.maximize_window()
-driver.get("https://google.com")
+driver.get("http://localhost:3000") 
+
+
+
 
 #Keeps the browser open for a moment
 time.sleep(3)
