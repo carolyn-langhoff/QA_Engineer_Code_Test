5839c960cd09de603501194b68d57cf1ed5697ce
carolyn <carolyn_langhoff@outlook.com>
2022-05-21 00:02:11 -0500

    commit

--- test_engineer/code_test.py
+++ test_engineer/code_test.py
@@ -3,13 +3,10 @@
 #Imports
 from codeop import CommandCompiler
 from msilib.schema import LaunchCondition, SelfReg
+from pickle import TRUE
 import time
 import pytest
-from typing import KeysView
-from urllib.request import urlopen
 from selenium import webdriver
-from setuptools import Command
-import urllib3
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.firefox import GeckoDriverManager
 
@@ -22,32 +19,45 @@ driver.maximize_window()
 driver.get("http://localhost:3000") 
 
 #Test ability to create a to-do list item
-#Xpath /html/body/div/div/form/input
+
+
+
+
 inputBlank = driver.find_element_by_xpath('/html/body/div/div/form/input')
 addItemButton = driver.find_element_by_xpath('/html/body/div/div/form/button')
 
 #Type new task into the inputBlank
-inputBlank.send_keys("Wash laundry")
+inputBlank.send_keys("Wash laundry") 
 addItemButton.click()
-inputBlank.send_keys("This proves that the application allows the user to create a new item.")
-addItemButton.click() 
+inputBlank.send_keys("Meal Prep") 
+addItemButton.click()
 
+time.sleep(5)
 
 #Test ability to check off or complete a to-do list item
+
+
 #Xpath /html/body/div/div/ul/li[1]/span
 checkBox = driver.find_element_by_xpath('/html/body/div/div/ul/li[1]/span')
 checkBox.click()
+ 
 time.sleep(5)
 
 #Test ability to uncheck a to-do list item
-checkBox = driver.find_element_by_xpath('/html/body/div/div/ul/li[4]/span') #Re-assigns the fourth list item to "checkBox" for the purposes of testing the ability to uncheck
+
+
+#Re-assigns the fourth list item to "checkBox" for the purposes of testing the ability to uncheck
+checkBox = driver.find_element_by_xpath('/html/body/div/div/ul/li[4]/span')
 checkBox.click() 
 time.sleep(5)
 
 #Test the ability to delete a to-do list item
+
+
 #Xpath /html/body/div/div/ul/li[1]/button
 deleteX = driver.find_element_by_xpath('/html/body/div/div/ul/li[1]/button')    
 deleteX.click() #Deletes the first item in the todo list
+
 
 #Keeps the browser open for a moment
 time.sleep(10)
